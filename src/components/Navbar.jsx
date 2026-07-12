0bfa1e1ecdb3003e214bfd32335417dc612515f0
BatchuUmaNagaSriAkhil <23ht1a4407@gmail.com>
2026-07-12 11:56:42 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css'
+function Navbar(){
+  return(
+    <nav className='navbar'>
+    <h1 style={{
+      color:"red",
+      // Tailwind css
+    }}>Placement Management System</h1>
+</nav>
+  );
+};
+export default Navbar;
